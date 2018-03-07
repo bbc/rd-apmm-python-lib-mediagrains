c2444017eb84a2e1d5c4bc92ef63c02e8fc74358
James P. Weaver <james.barrett@bbc.co.uk>
2018-03-07 10:02:29 +0000

    Added warranty disclaimer

--- mediagrains/grain_constructors.py
+++ mediagrains/grain_constructors.py
@@ -2,6 +2,9 @@
 #
 # Copyright 2018 British Broadcasting Corporation
 #
+# This software is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND
+#
 # This is an internal BBC tool and is not licensed externally
 # If you have received a copy of this erroneously then you do
 # not have permission to reproduce it.
