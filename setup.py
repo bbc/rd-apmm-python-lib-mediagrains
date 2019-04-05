4f4e2aad5a1106f66f62e010a90265dc3254905d
James Weaver <james.barrett@bbc.co.uk>
2019-04-05 11:46:17 +0100

    Make listing of modules in setup.py explicit

--- setup.py
+++ setup.py
@@ -19,30 +19,9 @@ from __future__ import print_function
 from setuptools import setup
 import os
 
-
-def is_package(path):
-    return (
-        os.path.isdir(path) and
-        os.path.isfile(os.path.join(path, '__init__.py'))
-        )
-
-
-def find_packages(path, base=""):
-    """ Find all packages in path """
-    packages = {}
-    for item in os.listdir(path):
-        dir = os.path.join(path, item)
-        if is_package(dir):
-            if base:
-                module_name = "%(base)s.%(item)s" % vars()
-            else:
-                module_name = item
-            packages[module_name] = dir
-            packages.update(find_packages(dir, module_name))
-    return packages
-
-
-packages = find_packages(".")
+packages = {
+    'mediagrains' : 'mediagrains'
+}
 package_names = packages.keys()
 
 packages_required = [
