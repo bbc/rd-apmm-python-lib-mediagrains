b9d17be3326ffd4934709c0a428c92c481963609
James Weaver <james.barrett@bbc.co.uk>
2019-10-28 16:46:57 +0000

    Grains can now be constructed with a coroutine to load their data element later

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -67,6 +67,7 @@ def attribute_and_pairs_of_grains_of_type_differing_only_in_one_attribute(grain_
         return grain_strat
 
 
+
 def suppress_deprecation_warnings(f):
     @wraps(f)
     def __inner(*args, **kwargs):
