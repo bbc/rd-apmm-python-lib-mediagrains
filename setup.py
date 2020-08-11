c98de5f56a6c99b91920e9a9a9d54deb71c46b35
Philip de Nier <philipn@rd.bbc.co.uk>
2020-08-11 16:10:40 +0100

    Version 2.14.1.post1

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ console_scripts = [
 ]
 
 setup(name="mediagrains",
-      version="2.14.1",
+      version="2.14.1.post1",
       python_requires='>=3.6.0',
       description="Simple utility for grain-based media",
       url='https://github.com/bbc/rd-apmm-python-lib-mediagrains',
